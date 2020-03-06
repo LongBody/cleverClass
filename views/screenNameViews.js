bf65798abdde6bc33bc5a4b01bbc651cbf26b40e
LongBody <nguyenthanhlong1120@gmail.com>
2020-03-06 12:52:06 +0700

    update

--- views/screenNameViews.js
+++ views/screenNameViews.js
@@ -85,23 +85,26 @@ view.showComponents = async function(screenName) {
                 }
 
                 // sign in with google 
-                firebase.auth().signInWithPopup(provider).then(function(result) {
-                    // This gives you a Google Access Token. You can use it to access the Google API.
-                    var token = result.credential.accessToken;
-                    // The signed-in user info.
-                    var user = result.user;
-                    console.log(user)
+                function googleSignInHandler() {
+
+                    firebase.auth().signInWithPopup(provider).then(function(result) {
+                        // This gives you a Google Access Token. You can use it to access the Google API.
+                        var token = result.credential.accessToken;
+                        // The signed-in user info.
+                        var user = result.user;
+                        console.log(user)
+                            // ...
+                    }).catch(function(error) {
+                        // Handle Errors here.
+                        var errorCode = error.code;
+                        var errorMessage = error.message;
+                        // The email of the user's account used.
+                        var email = error.email;
+                        // The firebase.auth.AuthCredential type that was used.
+                        var credential = error.credential;
                         // ...
-                }).catch(function(error) {
-                    // Handle Errors here.
-                    var errorCode = error.code;
-                    var errorMessage = error.message;
-                    // The email of the user's account used.
-                    var email = error.email;
-                    // The firebase.auth.AuthCredential type that was used.
-                    var credential = error.credential;
-                    // ...
-                })
+                    })
+                }
 
                 // async function forgetPasswordHandler(e) {
                 //     e.preventDefault()
